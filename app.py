7d3a3921566a15809f677ebf69a96626743514e0
Zach Gershkoff <zgershkoff@users.noreply.github.com>
2020-08-24 18:33:55 -0500

    Move from root to '/tasks'

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+basedir = "/tasks"
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
@@ -16,7 +18,7 @@ class Todo(db.Model):
 
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route(basedir + '/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
@@ -25,25 +27,25 @@ def index():
         try:
             db.session.add(new_task)
             db.session.commit()
-            return redirect('/')
+            return redirect(basedir + '/')
         except:
             return 'There was an issue adding your task'
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('index.html', tasks=tasks, basedir=basedir)
 
-@app.route("/delete/<int:id>")
+@app.route(basedir + "/delete/<int:id>")
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
 
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-        return redirect('/')
+        return redirect(basedir + '/')
     except:
         return 'There was a problem deleting that task'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
+@app.route(basedir + '/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
 
@@ -52,11 +54,11 @@ def update(id):
 
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect(basedir + '/')
         except:
             return 'There was an issue updating your task'
     else:
-        return render_template('update.html', task=task)
+        return render_template('update.html', task=task, basedir = basedir)
 
 
 
